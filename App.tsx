c9e85e57bf1ac6ed331edf6b63b7b95af864c71d
LeeSanghyun1212 <gustkd7125@gmail.com>
2025-09-07 18:29:16 +0900

    feat: 스플래시 화면 및 앱 아이콘 구현 완료

--- App.tsx
+++ App.tsx
@@ -6,15 +6,19 @@
  */
 
 import { NewAppScreen } from '@react-native/new-app-screen';
+import { useEffect } from 'react';
 import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
 import {
   SafeAreaProvider,
   useSafeAreaInsets,
 } from 'react-native-safe-area-context';
+import SplashScreen from 'react-native-splash-screen';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
-
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
   return (
     <SafeAreaProvider>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
@@ -29,7 +33,7 @@ function AppContent() {
   return (
     <View style={styles.container}>
       <NewAppScreen
-        templateFileName="App.tsx"
+        templateFileName='App.tsx'
         safeAreaInsets={safeAreaInsets}
       />
     </View>
