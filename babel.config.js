3caceb728bccb3c4c89ce817b879f9891c782305
김규리 <mandarining0918@gmail.com>
2025-09-08 22:51:07 +0900

    chore: import 경로를 위한 babel 설정

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,29 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
-  plugins: [
-    [
-      'module:react-native-dotenv',
-      {
-        moduleName: '@env',
-        path: '.env',
-      },
+    presets: ['module:@react-native/babel-preset'],
+    plugins: [
+        [
+            'module:react-native-dotenv',
+            {
+                moduleName: '@env',
+                path: '.env',
+            },
+        ],
+        [
+            'module-resolver',
+            {
+                root: ['./src'],
+                alias: {
+                    '@components': './src/components',
+                    '@pages': './src/pages',
+                    '@hooks': './src/hooks',
+                    '@utils': './src/utils',
+                    '@slices': './src/slices',
+                    '@assets': './src/assets',
+                    '@styles': './src/styles',
+                    '@store': './src/store',
+                    '@api': './src/api',
+                },
+            },
+        ]
     ],
-  ],
 };
