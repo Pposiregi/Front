f65b1b1ddda134464a816ef9fabba1e70df7b9aa
김규리 <mandarining0918@gmail.com>
2025-09-08 12:43:17 +0900

    Feat: Main용 펫, 걸음 진행 컴포넌트 생성

---
+++ src/components/PetAvatar.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Image, TouchableOpacity } from "react-native";
+
+type Props = { uri: string; expression: string; onPress?: () => void };
+
+/***
+ * PetAvatar Component
+ * @param uri - 펫 이미지 URL
+ * @param expression - 펫 감정 (현재 미사용)
+ * @param onPress - 클릭 시 호출되는 함수
+ */
+export const PetAvatar = ({ uri, expression, onPress }: Props) => {
+    return (
+        <TouchableOpacity onPress={onPress}>
+            <Image
+                source={{ uri }}
+                style={{ width: 250, height: 250 }}
+                resizeMode="contain"
+            />
+        </TouchableOpacity>
+    );
+};
