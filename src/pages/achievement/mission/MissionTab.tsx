8a93899a3e4eb108eebae720902be39d9e524cbf
LeeSanghyun1212 <gustkd7125@gmail.com>
2026-01-14 22:26:57 +0900

    chore : 오타 수정

--- src/pages/achievement/mission/MissionTab.tsx
+++ src/pages/achievement/mission/MissionTab.tsx
@@ -29,7 +29,7 @@ const MissionTab = () => {
         setErrorMessage(
           '미션 기록을 불러오지 못했어요. 잠시 후 다시 시도해주세요.'
         );
-        +setStatus('ERROR');
+        setStatus('ERROR');
       }
     };
 
