c4c1abcc85beebfc73cc47f6a2c16d0822848922
LeeSanghyun1212 <gustkd7125@gmail.com>
2026-03-13 08:19:25 +0900

    feat : 랭킹 필터 변경

--- src/api/rankingApi.ts
+++ src/api/rankingApi.ts
@@ -7,21 +7,14 @@ import type {
 export const getDailyStepRanking = async ({
   gender = 'ALL',
 }: GetDailyStepRankingRequest): Promise<DailyStepRankingResponse> => {
-  let url = '/ranking/summary';
-  const params: any = {};
+  const params = {
+    filter: gender.toUpperCase(),
+  };
 
-  // 남/여 필터 API 새로 수정되면 그거 가져다가 쓸 것
-  if (gender === 'MALE') {
-    url = '/users/rankings/daily-step/gender';
-    params.gender = 'male';
-  } else if (gender === 'FEMALE') {
-    url = '/users/rankings/daily-step/gender';
-    params.gender = 'female';
-  }
-
-  const { data } = await apiClient.get<DailyStepRankingResponse>(url, {
-    params,
-  });
+  const { data } = await apiClient.get<DailyStepRankingResponse>(
+    '/ranking/summary',
+    { params }
+  );
 
   if (!data || !Array.isArray(data.topRankings)) {
     throw new Error('부정확한 랭킹 데이터.');
